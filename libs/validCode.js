491300eda766cfd771a88fcc584cf4ad85f2f223
yoyo-user-awa <yoyos_email@163.com>
2026-01-22 21:53:19 +0800

    fix salt

--- libs/validCode.js
+++ libs/validCode.js
@@ -1,20 +1,14 @@
 function validCode(c){
-    const SALT_POOL = [
-            'XKJ', 'RTL', 'QPD', 'VHN', 'MGB',
-            'ZYX', 'WVU', 'TSR', 'QPO', 'NML',
-            'KJI', 'HGF', 'EDC', 'BAZ', 'YXV',
-            'WUT', 'SQR', 'PON', 'MLK', 'JIH',
-            'GFE', 'DCB', 'AZY', 'XWV', 'UTS',
-            'RQP', 'ONM', 'LKJ', 'IHG', 'FED'
-    ];
+    const pool = DatabaseMngr.getItem('pool') ?? [];
     let parts = c.split('-');
     if (parts[0].length == 6 && parts[1].length == 10 && parts[2].length == 3){
         let type = decodeAA26(parts[0]);
         let coin = decodeAA26(parts[1]);
         let salt = parts[2];
         if (!['VIP','NOR','PRI'].includes(type)) return {suc: false};
-        if (!SALT_POOL.includes(salt)) return {suc: false};
+        if (pool.includes(salt)) return {suc: false};
         if (!/^-?\d+$/.test(coin)) return {suc: false};
+        DatabaseMngr.addItem('pool',pool.concat([salt,]))
         return {
             suc: true,
             t: type,
